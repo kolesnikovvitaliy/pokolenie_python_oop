79c961787114de0539ec7fd1908a11da470e193c
Колесников Виталий <kolesnikovvitaliy@mail.ru>
2023-09-12 12:10:28 +0300

    Добавил решение 9_14_Декоратор_predicate

--- 9_Задачи_на_проектирование_классов/9_13_Класс_MultiKeyDict/class_MultiKeyDict.py
+++ 9_Задачи_на_проектирование_классов/9_13_Класс_MultiKeyDict/class_MultiKeyDict.py
@@ -1,3 +1,65 @@
 ''' Первый вариант решения'''
+from collections import UserDict
 
+
+class MultiKeyDict(UserDict):
+    def __init__(self, *args, **kwargs):
+        self._key = {}
+        super().__init__(*args, **kwargs)
+        self._key = {k: k for k in self.data.keys()}
+
+    def alias(self, key, alias):
+        if alias not in self.data:
+            self._key[alias] = key
+
+    def __getitem__(self, __key):
+        if __key in self._key:
+            return super().__getitem__(self._key[__key])
+        return super().__getitem__(__key)
+
+    def __setitem__(self, __key, __value):
+        if __key in self._key:
+            self.data[self._key[__key]] = __value
+        else:
+            self.data[__key] = __value
+
+    def __delitem__(self, __key):
+        new_key = [i for i in self._key.keys() if i != __key][0]
+        if new_key not in self.data:
+            del self._key[__key]
+            self._key[new_key] = new_key
+            self.data[new_key] = self.data.pop(__key)
+            if __key in self._key.values():
+                key_2 = [k for k, v in self._key.items() if v == __key][0]
+                self._key[key_2] = new_key
+        else:
+            self.data[__key] = self.data.pop(self._key[__key])
+            self._key[__key] = new_key
+            del self._key[new_key]
+            del self.data[__key]
 ''' Второй вариант решения'''    
+# from collections import UserDict
+
+
+# class MultiKeyDict(UserDict):
+#     def __init__(self, *args, **kwargs):
+#         self._aliases = {}
+#         super().__init__(*args, **kwargs)
+
+#     def alias(self, key, alias):
+#         self._aliases[alias] = key
+
+#     def __getitem__(self, key):
+#         return self.data.get(key) or self.data.get(self._aliases[key])
+
+#     def __setitem__(self, key, value):
+#         if key in self.data or key not in self._aliases:
+#             self.data[key] = value
+#         else:
+#             self.data[self._aliases[key]] = value
+
+#     def __delitem__(self, del_key):
+#         for alias_key, key in self._aliases.items():
+#             if key == del_key:
+#                 self.data[alias_key] = self.data[del_key]
+#         del self.data[del_key]
--- 9_Задачи_на_проектирование_классов/9_14_Декоратор_predicate/README.md
+++ 9_Задачи_на_проектирование_классов/9_14_Декоратор_predicate/README.md
@@ -109,12 +109,59 @@ print(is_less_than(3, 2))                     # False
 ## Примеры решений:
 * Первый вариант решения
 ```python
+class Predicate:
+    def __init__(self, func):
+        self.func = func
 
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __invert__(self):
+        def invert_func(*args, **kwargs):
+            return not self(*args, **kwargs)
+        return __class__(invert_func)
+
+    def __and__(self, other):
+        def and_func(*args, **kwargs):
+            return self(*args, **kwargs) and other(*args, **kwargs)
+        return __class__(and_func)
+
+    def __or__(self, other):
+        def or_func(*args, **kwargs):
+            return self(*args, **kwargs) or other(*args, **kwargs)
+        return __class__(or_func)
+
+
+def predicate(func):
+    return Predicate(func)
 ```
 * Второй вариант решения
 
 ```python
+class predicate:
+    def __init__(self, func):
+        self.func = func
 
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __invert__(self):
+        def not_func(*args, **kwargs):
+            return not self.func(*args, **kwargs)
+
+        return type(self)(not_func)
+
+    def __or__(self, other):
+        def or_func(*args, **kwargs):
+            return self.func(*args, **kwargs) or other.func(*args, **kwargs)
+
+        return type(self)(or_func)
+
+    def __and__(self, other):
+        def and_func(*args, **kwargs):
+            return self.func(*args, **kwargs) and other.func(*args, **kwargs)
+
+        return type(self)(and_func)
 ```
 
 
